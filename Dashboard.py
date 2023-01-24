fa1dae56f6b940cbe2176b23c8a320a380ae611e
Kenny La <kenny.la@outlook.com>
2023-01-25 00:20:23 +0100

    Increase heatmap radius and enable city slider for basic map

--- Dashboard.py
+++ Dashboard.py
@@ -23,6 +23,7 @@ df = df[(df['Bundesland'] == 'BAW') | (df['Bundesland'] == '')]
 
 # Display number of rows and columns
 st.subheader('Number of rows and columns')
+st.write("After deleting rows where Bundesland is not BAW or not empty")
 st.write(df.shape)
 
 # create an input for the user to select a city
@@ -30,7 +31,7 @@ city = st.selectbox('Select a city', df['Anbieterstadt'].unique())
 
 # Create a map with the selected city where the longitude and latitude are not 0
 st.subheader('Map')
-st.map(df[(df['lat'] != 0) & (df['lon'] != 0)][['lat', 'lon']])
+st.map(df[(df['Anbieterstadt']==city) & (df['lat'] != 0) & (df['lon'] != 0)][['lat', 'lon']])
 
 # Create a histogram with the number of courses per city
 st.subheader('Number of courses per city')
@@ -39,7 +40,7 @@ st.bar_chart(df['Anbieterstadt'].value_counts())
 # Create a heatmap with the number of courses per city
 st.subheader('Heatmap')
 st.pydeck_chart(pdk.Deck(
-    map_style='mapbox://styles/mapbox/light-v9',
+    map_style=None,
     initial_view_state=pdk.ViewState(
         latitude=48.783333,
         longitude=9.183333,
@@ -51,7 +52,7 @@ st.pydeck_chart(pdk.Deck(
             'HeatmapLayer',
             data=df[['lat', 'lon']],
             get_position='[lon, lat]',
-            radius=100,
+            radius=5000,
             extruded=True,
             pickable=True,
             elevation_scale=4,
@@ -72,7 +73,6 @@ st.write(df['Kursstadt'].value_counts())
 st.subheader('3D Map')
 st.pydeck_chart(pdk.Deck(
     map_style=None,
-    # map_style='mapbox://styles/mapbox/light-v9',
     initial_view_state=pdk.ViewState(
         latitude=48.783333,
         longitude=9.183333,
