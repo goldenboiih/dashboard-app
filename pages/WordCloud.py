e66ec87c421296ac7717106cc255a426e030c4b3
k_peikert <peikert.katrin@web.de>
2023-02-03 21:11:26 +0100

    working wordcloud

--- pages/WordCloud.py
+++ pages/WordCloud.py
@@ -1,41 +1,72 @@
 import streamlit as st
 import streamlit_wordcloud as wordcloud
-from Dashboard import df
 
-df_copy = df.copy()
+import pandas as pd
+from wordcloud import WordCloud
 
-# 1. replace :, with " "
-# 2. detect all nouns -> remove those with "https://" 
-# 3. [["word1", "word2"], ["word2", "word3"]]
-# 4. Throw some TF-IDF on it
+NUMBER_OF_WORDS_IN_CLOUD = 15
 
-"""
-    ## Testing WordCloud
-    descriptions = [i for i in list(df["Kursbeschreibung"]) if i != ""]
-    joined_descriptions = " ".join(descriptions)
-    doc = nlp(joined_descriptions)
-    nouns =[w for w in doc]# if w.pos_ in[ "NOUN", "PROPN"]]
 
-    word_counts = list()
-    for n in nouns:
-        word_counts.append({"text":n, "value": nouns.count(n)})
-        if nouns.count(n) > 1:
-            print(n, nouns.count(n))
-"""
+######## calculate provider number of courses ############
+with open('resources/Top20k_valid.JSON', 'r', encoding='utf-8') as file:
+    df_copy = pd.read_json(file)
+provider_names = set(df_copy["Veranstaltername"])
+provider_number_of_courses = dict()
+for provider in provider_names:
+    provider_desc= df_copy.loc[df_copy['Veranstaltername'] == provider]["Kursbeschreibung"]
+    if provider == "":
+        provider_number_of_courses['no provider given'] = len(provider_desc)
+    else:
+        provider_number_of_courses[provider] = len(provider_desc)
 
-words = [
-    dict(text="Robinhood", value=16000, country="US", industry="Cryptocurrency"),
-    dict(text="Personio", value=8500,  country="DE", industry="Human Resources"),
-    dict(text="Boohoo", value=6700, country="UK", industry="Beauty"),
-    dict(text="Deliveroo", value=13400, country="UK", industry="Delivery"),
-    dict(text="SumUp", value=8300,  country="UK", industry="Credit Cards"),
-    dict(text="CureVac", value=12400, country="DE", industry="BioPharma"),
-    dict(text="Deezer", value=10300,country="FR", industry="Music Streaming"),
-    dict(text="Eurazeo", value=31, country="FR", industry="Asset Management"),
-    dict(text="Drift", value=6000,  country="US", industry="Marketing Automation"),
-    dict(text="Twitch", value=4500,  country="US", industry="Social Media"),
-    dict(text="Plaid", value=5600,  country="US", industry="FinTech"),
-]
-return_obj = wordcloud.visualize(words, tooltip_data_fields={
-    'text':'Company', 'value':'Mentions', 'country':'Country of Origin', 'industry':'Industry'
-    }, per_word_coloring=False)
+#########################################################
+
+data_all = pd.read_csv("resources/tf_idf_all_only_nouns.csv").rename(columns={'Unnamed: 0':"words"})
+dict_data = data_all.to_dict()
+
+
+
+@st.cache
+def word_cloud_together():
+    word_count= dict()
+    for i in range(0,NUMBER_OF_WORDS_IN_CLOUD):
+        word_count[dict_data['words'][i]] = dict_data['TF-IDF'][i]*100
+    all_words = WordCloud(width=600, height=300).fit_words(word_count)
+    return all_words.to_array()
+
+with st.container():
+    st.header("Wordcloud for all Courses together:")
+    st.image(word_cloud_together())
+st.markdown("""---""")
+#########################################################
+## For each Provider:
+@st.cache
+def get_all_provider():
+    data_provider = pd.read_csv("resources/tf_idf_each_provider.csv").rename(columns={'Unnamed: 0':"words"})
+    provider_names = set(data_provider["provider"])
+    all_word_clouds = dict()
+    for name in provider_names: 
+        dict_data  = data_provider.loc[data_provider["provider"] == name].head(10)[["words", "TF-IDF"]]
+        dict_data = dict_data.to_dict()
+        word_cloud = dict()
+        counter = 0
+        for id_key in dict_data["words"].keys():
+            word_cloud[dict_data['words'][id_key]] = dict_data['TF-IDF'][id_key]
+            counter += 1
+            if counter == NUMBER_OF_WORDS_IN_CLOUD:
+                break
+        all_word_clouds[name] = word_cloud
+    return all_word_clouds
+
+all_word_clouds = get_all_provider()
+with st.container():
+    st.header("Wordcloud for a single Course Provider:")
+    option = st.selectbox('For which Course would you like to see a Wordcloud?',(all_word_clouds.keys()))
+
+
+    st.subheader(option)
+    st.write("The Provider has "+ str(provider_number_of_courses[option])+" courses.")
+    wc = WordCloud(width=600, height=300).fit_words(all_word_clouds[option])
+    st.image(wc.to_array())
+
+
--- testing_wordcloud.py
+++ testing_wordcloud.py
@@ -2,26 +2,31 @@ import pandas as pd
 import re
 import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
-import json
+
+# Barchart(s): Andy
+# Heatmap: Kenny
+# Wordcloud: Katrin
+# General Overview: #Anbieter #Kurse #Städte
+# Kurse nach Monat
+
 
 nlp = spacy.load("de_core_news_sm")
 with open('resources/Top20k_valid.JSON', 'r', encoding='utf-8') as file:
     df = pd.read_json(file)
 
-
-
-
-
 def clean_data(data:list, allowed_word_types = []):
     """
     # [ "NOUN", "PROPN"]
     """
+    new_data = []
     for sentence in data:
         sentence, n = re.subn('[:]', ' ', sentence)
         sentence, n = re.subn('[.!?]', ' . ', sentence)
         if allowed_word_types != []:
-            sentence = " ".join([w .text for w in nlp(sentence) if w.pos_ in allowed_word_types])
-    return data
+            sentence = " ".join([w.text for w in nlp(sentence) if w.pos_ in allowed_word_types])
+        new_data.append(sentence)
+            
+    return new_data
 
 
 def calculate_tf_idf_score(descriptions: list, allowed_word_types = []):
@@ -29,14 +34,104 @@ def calculate_tf_idf_score(descriptions: list, allowed_word_types = []):
     Calculates the TF-IDF Score for a list of sentences, after cleaning the data
     """
     descriptions = clean_data(descriptions, allowed_word_types)
-    tfIdfVectorizer=TfidfVectorizer(use_idf=True)
-    tfIdf = tfIdfVectorizer.fit_transform(descriptions)
-    df = pd.DataFrame(tfIdf[0].T.todense(), index=tfIdfVectorizer.get_feature_names(), columns=["TF-IDF"])
-    df = df.sort_values('TF-IDF', ascending=False)
+    try:
+        tfIdfVectorizer=TfidfVectorizer(use_idf=True, min_df=1)
+    
+        tfIdf = tfIdfVectorizer.fit_transform(descriptions)
+        df = pd.DataFrame(tfIdf[0].T.todense(), index=tfIdfVectorizer.get_feature_names(), columns=["TF-IDF"])
+        df = df.sort_values('TF-IDF', ascending=False)
     #print (df.head(25))
-    return df
+        return df
+    except:
+        print(descriptions)
+        for i in descriptions:
+            
+            print(TfidfVectorizer().build_analyzer()(i))
+        return None
 
 
-descriptions_all = [i for i in list(df["Kursbeschreibung"]) if i != ""]
-tf_idf_all = calculate_tf_idf_score(descriptions_all, ["NOUN", "PROPN"])
-tf_idf_all.to_csv('resources/tf_idf_all.csv')
+## Get all data 
+def get_tf_idf_all_together():
+    """
+    Calculates the TF-IDF score for all "Kursbeschreibungen" in the dataset
+    """
+    descriptions_all = [i for i in list(df["Kursbeschreibung"]) if i != ""][1:10]
+    tf_idf_all = calculate_tf_idf_score(descriptions_all, ["NOUN", "PROPN"])
+    tf_idf_all.to_csv('resources/tf_idf_all_only_nouns.csv')
+
+  
+def load_all_data():   
+    """
+    Testfunction for loading the data and shaping it for the wordcloud
+    """ 
+    data = pd.read_csv("resources/tf_idf_all.csv").rename(columns={'Unnamed: 0':"words"})
+    dict_data = data.to_dict()
+    print(dict_data.keys())
+    word_cloud = list()
+    for i in range(0,15):
+        word_cloud.append({"text": dict_data['words'][i], "value": dict_data['TF-IDF'][i] })
+    print(word_cloud)
+
+####
+
+def get_descriptions_for_provider():
+    """ 
+    Obtains all non-empty "Kursbeschreibungen" from the dataset for each provider
+    """
+    provider_names = set(df["Veranstaltername"])
+    provider_descriptions = dict()
+    tmp_df = df[df['Kursbeschreibung'] != ""]
+    for provider in provider_names:
+        provider_desc= tmp_df.loc[tmp_df['Veranstaltername'] == provider]["Kursbeschreibung"]
+        
+        if len(provider_desc) > 1:
+            if provider == "":
+                provider_descriptions["no provider given"] = provider_desc
+            else: 
+                provider_descriptions[provider] = provider_desc
+        else:
+            print(provider)
+
+    return provider_descriptions
+
+
+def get_all_provider():
+    """ 
+    Calculates the TF-IDF score for all "kursbeschreibungen" belonging 
+    to each "Veranstalter" and writes it into a csv file
+    """
+    all_provider_descriptions = get_descriptions_for_provider()
+    frames = []
+    for prov in all_provider_descriptions.keys():
+        tmp_tf_idf_score = calculate_tf_idf_score(all_provider_descriptions[prov],  [])
+        tmp_tf_idf_score["provider"] = str(prov)
+        frames.append(tmp_tf_idf_score)
+    result = pd.concat(frames)
+    result.to_csv('resources/tf_idf_each_provider_all_wordtypes.csv')
+
+#get_tf_idf_all_together()
+get_all_provider()
+
+
+
+"""
+data_provider = pd.read_csv("resources/tf_idf_each_provider.csv").rename(columns={'Unnamed: 0':"words"})
+provider_names = set(data_provider["provider"])
+all_word_clouds = dict()
+for name in provider_names:
+    
+    dict_data  = data_provider.loc[data_provider["provider"] == name].head(10)[["words", "TF-IDF"]]
+    dict_data = dict_data.to_dict()
+    word_cloud = list()
+    counter = 0
+    for id_key in dict_data["words"].keys():
+        word_cloud.append({"text": dict_data['words'][id_key],
+                           "value": dict_data['TF-IDF'][id_key] })
+        counter += 1
+        if counter == 15:
+            break
+    all_word_clouds[name] = word_cloud
+
+print(all_word_clouds)
+"""
+
