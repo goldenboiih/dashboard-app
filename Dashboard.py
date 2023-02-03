f8b82290e1f1fcdc29832732843fd26b8ff35ceb
scholl123 <86308557+scholl123@users.noreply.github.com>
2023-02-03 13:31:33 +0100

    added pie chart and tab navigation.

--- Dashboard.py
+++ Dashboard.py
@@ -16,88 +16,99 @@ df.dropna(axis=1, inplace=True)
 df.rename(columns={'Latitude': 'lat', 'Longitude': 'lon'}, inplace=True)
 
 st.title('Kurse - Dashboard')
-df
 
 # Drop rows where Bundesland is not BAW or not empty
 df = df[(df['Bundesland'] == 'BAW') | (df['Bundesland'] == '')]
 
-# Display number of rows and columns
-st.subheader('Number of rows and columns')
-st.write("After deleting rows where Bundesland is not BAW or not empty")
-st.write(df.shape)
+# define tabs
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(['Start', 'City Map', 'Courses per city', 'Heatmap',
+                                             'Courses per provider', '3D Map'])
 
-# create an input for the user to select a city
-city = st.selectbox('Select a city', df['Anbieterstadt'].unique())
+with tab1:
+    df
+    # Display number of rows and columns
+    st.subheader('Number of rows and columns')
+    st.write("After deleting rows where Bundesland is not BAW or not empty")
+    st.write(df.shape)
 
-# Create a map with the selected city where the longitude and latitude are not 0
-st.subheader('Map')
-st.map(df[(df['Anbieterstadt']==city) & (df['lat'] != 0) & (df['lon'] != 0)][['lat', 'lon']])
+with tab2:
+    # create an input for the user to select a city
+    city = st.selectbox('Select a city', df['Anbieterstadt'].unique())
 
-# Create a histogram with the number of courses per city
-st.subheader('Number of courses per city')
-st.bar_chart(df['Anbieterstadt'].value_counts())
+    # Create a map with the selected city where the longitude and latitude are not 0
+    st.subheader('Map')
+    st.map(df[(df['Anbieterstadt'] == city) & (df['lat'] != 0) & (df['lon'] != 0)][['lat', 'lon']])
 
-# Create a heatmap with the number of courses per city
-st.subheader('Heatmap')
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=48.783333,
-        longitude=9.183333,
-        zoom=7,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-            'HeatmapLayer',
-            data=df[['lat', 'lon']],
-            get_position='[lon, lat]',
-            radius=5000,
-            extruded=True,
-            pickable=True,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
+with tab3:
+    # Create a histogram with the number of courses per city
+    st.subheader('Number of courses per city')
+    st.bar_chart(df['Anbieterstadt'].value_counts())
+
+    # List number of courses per city
+    st.subheader('Number of courses per Kursstadt')
+    # st.pyplot(df['Kursstadt'].value_counts(normalize=True).head(10).plot.pie())
+
+with tab4:
+    # Create a heatmap with the number of courses per city
+    st.subheader('Heatmap')
+    st.pydeck_chart(pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=48.783333,
+            longitude=9.183333,
+            zoom=7,
+            pitch=50,
         ),
-    ],
-))
+        layers=[
+            pdk.Layer(
+                'HeatmapLayer',
+                data=df[['lat', 'lon']],
+                get_position='[lon, lat]',
+                radius=5000,
+                extruded=True,
+                pickable=True,
+                elevation_scale=4,
+                elevation_range=[0, 1000],
+            ),
+        ],
+    ))
 
-# List number of courses per provider
-st.subheader('Number of courses per Veranstaltername')
-st.write(df['Veranstaltername'].value_counts())
+with tab5:
+    # List number of courses per provider
+    st.subheader('Number of courses per Veranstaltername')
+    fig = df['Veranstaltername'].value_counts(normalize=True).head(10).plot(kind='pie').figure
+    st.pyplot(fig)
 
-# List number of courses per city
-st.subheader('Number of courses per Kursstadt')
-st.write(df['Kursstadt'].value_counts())
-
-# Create 3d map with number of courses per city
-st.subheader('3D Map')
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=48.783333,
-        longitude=9.183333,
-        zoom=7,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=df[['lat', 'lon']],
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
+with tab6:
+    # Create 3d map with number of courses per city
+    st.subheader('3D Map')
+    st.pydeck_chart(pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=48.783333,
+            longitude=9.183333,
+            zoom=7,
+            pitch=50,
         ),
-        pdk.Layer(
-            'HexagonLayer',
-            data=df[['lat', 'lon']],
-            get_position='[lon, lat]',
-            radius=4000,
-            elevation_scale=4,
-            elevation_range=[500, 20000],
-            pickable=True,
-            extruded=True,
-        ),
-    ],
-))
+        layers=[
+            pdk.Layer(
+                'ScatterplotLayer',
+                data=df[['lat', 'lon']],
+                get_position='[lon, lat]',
+                get_color='[200, 30, 0, 160]',
+                get_radius=200,
+            ),
+            pdk.Layer(
+                'HexagonLayer',
+                data=df[['lat', 'lon']],
+                get_position='[lon, lat]',
+                radius=4000,
+                elevation_scale=4,
+                elevation_range=[500, 20000],
+                pickable=True,
+                extruded=True,
+            ),
+        ],
+    ))
 
 # Run with streamlit run Dashboard.py
--- main.py
+++ main.py
@@ -16,30 +16,32 @@ with open('resources/Top20k_valid.JSON', 'r', encoding='utf-8') as file:
     df = pd.read_json(file)
 
 if __name__ == '__main__':
-    print(df.head())
-    print(df.columns)
-    print(df.shape)
+    # print(df.head())
+    # print(df.columns)
+    # print(df.shape)
 
     # Ten columns wit NaN values
     df.dropna(axis=1, inplace=True)
-    print(df.columns)
-    print(df.shape)
+    # print(df.columns)
+    # print(df.shape)
 
     # Drop rows where Bundesland is not BAW
     df = df[(df['Bundesland'] == 'BAW') | (df['Bundesland'] == '')]
-    print(df.shape)
+    # print(df.shape)
 
     # Only one Zulieferer
-    print(df['Zulieferername'].unique())
-    print(df['Zuliefererid'].unique())
+    # print(df['Zulieferername'].unique())
+    # print(df['Zuliefererid'].unique())
 
     # Rename Latitude and Longitude
     df.rename(columns={'Latitude': 'lat', 'Longitude': 'lon'}, inplace=True)
 
     # Check rows where lat and lon are unique
     df.drop_duplicates(subset=['lat', 'lon'], inplace=True)
-    print(df.shape)
+    # print(df.shape)
 
     # Drop rows where Kursstrasse is not unique
     df.drop_duplicates(subset=['Kursstrasse'], inplace=True)
-    print(df.shape)
+    # print(df.shape)
+    fig = df['Veranstaltername'].value_counts(normalize=True).head(10).plot.pie().figure
+    fig.show()
